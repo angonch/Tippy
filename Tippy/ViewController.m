66817c8958828291a86f4bc135236b08172df420
angonch <angonch@cs.washington.edu>
2020-08-18 16:56:30 -0700

    UI changes - green background and animations to make tip/total appear when done typing bill

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UIView *calculationView;
 
 @end
 
@@ -40,6 +41,23 @@
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
+        
+        self.calculationView.alpha = 0;
+    }];
+}
+- (IBAction)onEditingEnd:(id)sender {
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -= 30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        
+        self.calculationView.alpha = 1;
+    }];
 }
 
 @end

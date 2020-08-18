ef96de9e4cc4fbba234991e48c9e00dd5d559529
angonch <angonch@cs.washington.edu>
2020-08-18 16:01:12 -0700

    - update tip and total when bill is changed

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -22,6 +27,19 @@
 - (IBAction)onTap:(id)sender {
     NSLog(@"Dismiss Keyboard");
     [self.view endEditing:YES]; // java: this.view.endEditing(true);
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
 @end
